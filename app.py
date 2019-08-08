308038fcfa26c459e59ddbcb43e1b29c9465b6a7
Ubuntu <ubuntu@ip-172-31-91-50.ec2.internal>
2019-08-08 14:40:12 +0000

    text based indexing

--- app.py
+++ app.py
@@ -1,18 +1,44 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+import pymongo
 
 app = Flask(__name__)
+
+app.config['MONGO_URI'] = 'mongodb+srv://admin:Madeforadmin1@myfirstcluster-bbdvy.mongodb.net/cookbook?retryWrites=true&w=majority'
+app.config["MONGO_DBNAME"] = 'cookbook'
+mongo=PyMongo(app)
+
 
 
 @app.route('/')
 def index():
-    user = os.environ.get('USER')
-    password = os.environ.get('PASSWORD')
-    return render_template('index.html', user=user, password=password)
+    return render_template('index.html', recipes=mongo.db.recipes.find(), categories=mongo.db.categories.find())
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/search', methods=['POST'])
+def search():
+    
+    ''' 
+    The below line of code creates an index on each of the fields in the collection,
+    allowing us to perform a text search on them
+    '''
+    # mongo.db.recipes.create_index([('description', 'text'), ('name','text'), ('ingredients', 'text')])
+    
+    
+    ''' 
+    The below line of code drops all indexes on a collection.
+    '''
+    # mongo.db.recipes.drop_indexes()
+    
+    query = request.form.get('query')
+    recipes = mongo.db.recipes.find({'$text': {'$search': query}})
+    return render_template('recipes.html', recipes=recipes, type='searched', query=query)
+
+@app.route('/category/<type>')
+def recipe_type(type):
+    recipes=mongo.db.recipes.find({'category': type})
+    return render_template('recipes.html', recipes=recipes, type=type)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
